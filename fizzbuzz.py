111c9e2381f2dfff25c9d7be08cd4338c009efaa
Aaron Robson <ARobsonSE@hotmail.co.uk>
2016-07-06 17:45:04 +0100

    Using 2 space indentation rather than tabs.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python
 
 def DivisableFuncFactory(divider):
-	def Divisable(num):
-		'''Is number divisable by %d?
-		''' % divider
-		return num % divider == 0
-	return Divisable
+  def Divisable(num):
+    '''Is number divisable by %d?
+    ''' % divider
+    return num % divider == 0
+  return Divisable
 
 def GenericFuncFactory(conditions, sep=None, allMatches=True):
-	#Must evaluate generators as otherwise function will only work the first time; after which the generator will be finished.
-	conditions = tuple(conditions)
+  #Must evaluate generators as otherwise function will only work the first time; after which the generator will be finished.
+  conditions = tuple(conditions)
 
-	if sep == None:
-		sep = ''
+  if sep == None:
+    sep = ''
 
-	def GenericFunc(num):
-		output = tuple(value for predicate, value in conditions if predicate(num))
-		if output:
-			if allMatches:
-				return sep.join(output)
-			else:
-				return output[0]
-		else:
-			return num
+  def GenericFunc(num):
+    output = tuple(value for predicate, value in conditions if predicate(num))
+    if output:
+      if allMatches:
+        return sep.join(output)
+      else:
+        return output[0]
+    else:
+      return num
 
-	return GenericFunc
+  return GenericFunc
 
 def FizzBuzzFunc(sep=None):
-	dividerConditions = ((3, 'Fizz'), (5, 'Buzz'))
-	conditions = ((DivisableFuncFactory(predicateNum), value) for predicateNum, value in dividerConditions)
-	return GenericFuncFactory(conditions, sep)
+  dividerConditions = ((3, 'Fizz'), (5, 'Buzz'))
+  conditions = ((DivisableFuncFactory(predicateNum), value) for predicateNum, value in dividerConditions)
+  return GenericFuncFactory(conditions, sep)
 
 def GenericLoop(func, min, max, step=1):
-	min, max = sorted([min, max])
-	return (func(i) for i in range(min, max+1, step))
+  min, max = sorted([min, max])
+  return (func(i) for i in range(min, max+1, step))
 
 def FizzBuzzLoop(min=1, max=100, sep=None):
-	return GenericLoop(FizzBuzzFunc(sep), min, max)
+  return GenericLoop(FizzBuzzFunc(sep), min, max)
 
 if __name__ == "__main__":
-	for line in FizzBuzzLoop():
-		print(line)
-	input('\nPress Enter to Close:')
+  for line in FizzBuzzLoop():
+    print(line)
+  input('\nPress Enter to Close:')
