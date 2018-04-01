a5967d736ad9efe5dfab2ebbd2d36ac862c72609
Aaron Robson <ARobsonSE@hotmail.co.uk>
2018-04-01 18:25:14 +0100

    Removing sep option for clarity.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,33 +3,30 @@
 def DivisableFuncFactory(divider):
     return lambda num: num % divider == 0
 
-def GenericFuncFactory(conditions, sep=None):
+def GenericFuncFactory(conditions):
     #Must evaluate generators as otherwise function will only work the first time; after which the generator will be finished.
     conditions = tuple(conditions)
-
-    if sep == None:
-        sep = ''
 
     def GenericFunc(num):
         output = tuple(value for predicate, value in conditions if predicate(num))
         if output:
-            return sep.join(output)
+            return ''.join(output)
         else:
             return str(num)
 
     return GenericFunc
 
-def FizzBuzzFunc(sep=None):
+def FizzBuzzFunc():
     dividerConditions = ((3, 'Fizz'), (5, 'Buzz'))
     conditions = ((DivisableFuncFactory(predicateNum), value) for predicateNum, value in dividerConditions)
-    return GenericFuncFactory(conditions, sep)
+    return GenericFuncFactory(conditions)
 
 def GenericLoop(func, min, max, step=1):
     min, max = sorted([min, max])
     return (func(i) for i in range(min, max+1, step))
 
-def FizzBuzzLoop(min=1, max=100, sep=None):
-    return GenericLoop(FizzBuzzFunc(sep), min, max)
+def FizzBuzzLoop(min=1, max=100):
+    return GenericLoop(FizzBuzzFunc(), min, max)
 
 if __name__ == "__main__":
     for line in FizzBuzzLoop():
