e6f0e56f26646d7747d1077f541d24e682a27356
Aaron Robson <ARobsonSE@hotmail.co.uk>
2018-04-01 19:36:12 +0100

    camelCasing all functions.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,7 +5,7 @@ from functools import partial
 def isDivisible(numerator, denominator):
     return numerator % denominator == 0
 
-def ApplyConditions(num, conditions):
+def applyConditions(num, conditions):
     output = [value for predicate, value in conditions if predicate(num)]
     if output:
         return ''.join(output)
@@ -16,9 +16,9 @@ _conditions = [
     (partial(isDivisible, denominator=3), 'Fizz'),
     (partial(isDivisible, denominator=5), 'Buzz')
 ]
-fizzBuzz = partial(ApplyConditions, conditions=_conditions)
+fizzBuzz = partial(applyConditions, conditions=_conditions)
 
-def FizzBuzzLoop(min=1, max=100):
+def fizzBuzzLoop(min=1, max=100):
     return map(fizzBuzz, range(min, max+1))
 
 if __name__ == "__main__":
