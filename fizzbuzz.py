9090961aa7e5a50a5772182e0cfb9df5b3e1203a
Aaron Robson <AaronRobson@users.noreply.github.com>
2018-12-15 18:38:20 +0000

    Resolve flake8 code style issues

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,8 +2,10 @@
 
 from functools import partial
 
+
 def isDivisible(numerator, denominator):
     return numerator % denominator == 0
+
 
 def applyConditions(num, conditions):
     output = [value for predicate, value in conditions if predicate(num)]
@@ -12,14 +14,17 @@ def applyConditions(num, conditions):
     else:
         return str(num)
 
+
 _conditions = [
     (partial(isDivisible, denominator=3), 'Fizz'),
     (partial(isDivisible, denominator=5), 'Buzz')
 ]
 fizzBuzz = partial(applyConditions, conditions=_conditions)
 
+
 def fizzBuzzLoop(min=1, max=100):
     return map(fizzBuzz, range(min, max+1))
+
 
 if __name__ == "__main__":
     for line in fizzBuzzLoop():
