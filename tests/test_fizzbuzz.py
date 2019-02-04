0330e8f81b1efe70b29d67c81372a38cc3b3cfcb
Aaron Robson <AaronRobson@users.noreply.github.com>
2019-02-04 22:28:03 +0000

    testing generator

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import unittest
+from itertools import islice
 
 import fizzbuzz
 
@@ -15,6 +16,25 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(f(4), '4')
         self.assertEqual(f(5), 'Buzz')
         self.assertEqual(f(15), 'FizzBuzz')
+
+
+class TestFizzBuzzLoop(unittest.TestCase):
+
+    def test(self):
+        expected = [
+            '1',
+            '2',
+            'Fizz',
+            '4',
+            'Buzz',
+        ]
+        actual = take(len(expected), fizzbuzz.fizzBuzzLoop())
+        self.assertEqual(expected, actual)
+
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
 
 
 if __name__ == "__main__":
